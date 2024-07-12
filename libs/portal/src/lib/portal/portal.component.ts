2e62aaee57d12e784b0e0b8a6a99717a540994cd
Edwin Arias <earias10@slb.com>
2024-07-12 06:56:38 -0500

    add portal scaffolding control

---
+++ libs/portal/src/lib/portal/portal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-portal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './portal.component.html',
+  styleUrl: './portal.component.css',
+})
+export class PortalComponent {}
