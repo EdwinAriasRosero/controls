d5449c0e9de383f92b11b8d0555e73377603a2bf
Edwin Arias <earias10@slb.com>
2024-07-18 17:18:48 -0500

    repository

--- libs/ngrx/repository-webapi/jest.config.ts
+++ libs/ngrx/repository-webapi/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'repository-webapi',
+  displayName: 'ngrx-repository-webapi',
   preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   coverageDirectory: '../../../coverage/libs/ngrx/repository-webapi',
--- libs/ngrx/repository/jest.config.ts
+++ libs/ngrx/repository/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'repository',
+  displayName: 'ngrx-repository',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   coverageDirectory: '../../coverage/libs/repository',
